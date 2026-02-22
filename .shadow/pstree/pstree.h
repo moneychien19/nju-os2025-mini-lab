aa273f6c67578903a1e7a858dcd8d60f9ace030d
tracer-nju <tracer@nju.edu.cn>
2026-02-22 16:19:54 +0000

    Linux agent 6.8.0-94-generic #96-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan  9 20:18:23 UTC 2026 aarch64 aarch64 aarch64 GNU/Linux  16:19:54 up 6 days, 17:50,  5 users,  load average: 0.47, 0.20, 0.12

--- .shadow/pstree/pstree.h
+++ .shadow/pstree/pstree.h
@@ -4,8 +4,20 @@ typedef struct {
   char comm[256];
 } ProcessInfo;
 
+typedef struct {
+  ProcessInfo info;
+  struct ProcessNode **children;
+  size_t child_count;
+  size_t child_cap;
+} ProcessNode;
+
 int* get_all_pids();
 int get_process_info(int pid, ProcessInfo *info);
+ProcessNode* build_tree(int *pids);
+void print_tree(ProcessNode *node, const char *prefix, int is_last);
+
 char* find_comm(const char *stat_buf);
 int find_ppid(const char *stat_buf);
 int is_all_digits(const char *s);
+int get_max_pid();
+void add_child(ProcessNode *parent, ProcessNode *child);
